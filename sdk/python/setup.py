4b5424c5bca50bd7dcb9366e483ea5f53730ed85
Cocabadger <victor-heavy.8t@icloud.com>
2025-10-07 11:00:20 +0200

    chore: Bump Python SDK to v0.2.0

--- sdk/python/setup.py
+++ sdk/python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="saferun",
-    version="0.1.0",
+    version="0.2.0",
     description="Python SDK for SafeRun API",
     packages=find_packages(include=["saferun", "saferun.*"]),
     install_requires=[
